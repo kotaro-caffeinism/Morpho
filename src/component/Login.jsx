116b723d6d94dab2751da03cd4eb7969eac0e9fc
Kotaro <hideandhide35@gmail.com>
2022-08-28 19:16:06 +0900

    improved login and signup page

--- src/component/Login.jsx
+++ src/component/Login.jsx
@@ -1,18 +1,30 @@
+import React, { useState } from "react";
 import { auth } from "../firebase";
 import { useNavigate, Navigate } from "react-router-dom";
 
 const Login = () => {
   const navigate = useNavigate();
+  const [error, setError] = useState("");
   const handleSubmit = async (event) => {
+    // event.preventDefault();
+    // const { email, password } = event.target.elements;
+    // await auth.signInWithEmailAndPassword(email.value, password.value);
+    // navigate("/");
     event.preventDefault();
     const { email, password } = event.target.elements;
-    await auth.signInWithEmailAndPassword(email.value, password.value);
-    navigate("/");
+    try {
+      await auth.signInWithEmailAndPassword(email.value, password.value);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
   };
 
   return (
     <div>
       <h1>ログイン</h1>
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <form onSubmit={handleSubmit}>
         <div>
           <label>メールアドレス</label>
--- src/component/SignUp.jsx
+++ src/component/SignUp.jsx
@@ -1,15 +1,25 @@
+import React, { useState } from "react";
 import { firebaseConfig, auth } from "../firebase";
+import { useNavigate, Navigate } from "react-router-dom";
 
 const SignUp = () => {
-  const handleSubmit = (event) => {
-    console.log(firebaseConfig);
+  const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const handleSubmit = async (event) => {
     event.preventDefault();
     const { email, password } = event.target.elements;
-    auth.createUserWithEmailAndPassword(email.value, password.value);
+    try {
+      await auth.createUserWithEmailAndPassword(email.value, password.value);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setError(error.message);
+    }
   };
   return (
     <div>
       <h1>ユーザ登録</h1>
+      {error && <p style={{ color: "red" }}>{error}</p>}
       <form onSubmit={handleSubmit}>
         <div>
           <label>メールアドレス</label>
