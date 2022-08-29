c17986459e733f575604c0c055ddfdb08153d770
Kotaro <hideandhide35@gmail.com>
2022-08-29 09:05:31 +0900

    tring connect

--- seeds/sample-data.js
+++ seeds/sample-data.js
@@ -5,11 +5,11 @@
 exports.seed = async function (knex) {
   await knex("result").del();
   await knex.schema.raw("SELECT SETVAL ('result_id_seq', 1, false)");
-  await knex("result").insert([
-    {
-      id: 1,
-      code: "function returnVal (val) { return val };",
-      isPassed: true,
-    },
-  ]);
+  // await knex("result").insert([
+  //   {
+  //     id: 1,
+  //     code: "function returnVal (val) { return val };",
+  //     isPassed: true,
+  //   },
+  // ]);
 };
